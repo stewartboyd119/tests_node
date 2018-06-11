456062773bb202d84479881ae5dbf31be35841f0
Stewart Boyd <stewartboyd1988@gmail.com>
2018-06-11 19:07:21 -0400

    another commit

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,28 @@
 var add = (a, b) =>  a + b ;
+var asyncAdd = (a, b, callback) => {
+    setTimeout(() => {
+        result = a + b;
+        callback(result);
+    }, 1000);
+}
 var square = (a) => a * a;
+var asyncSquare = (a, callback) => {
+    setTimeout(() => {
+        result = a * a;
+        callback(result);
+    }, 1000);
+}
+var setName = (user, fullName) => {
+    var splitName = fullName.split(" "); 
+    user.firstName = splitName[0];
+    user.lastName = splitName[1];
+    return user;
+};
 
 module.exports = {
     add,
-    square
+    square,
+    setName,
+    asyncAdd,
+    asyncSquare
 }
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,32 @@
+const expect = require('expect');
 const utils = require('./utils');
 it('should add two numbers' , () => {
     var result = utils.add(2, 3);
     var expectedResult = 5;
-    if (result != expectedResult) {
-        throw new Error(`${result} != ${expectedResult}`);
-    }
+    expect(result).toBe(expectedResult);
 })
 it('should square a number' , () => {
     var result = utils.square(3);
     var expectedResult = 9;
-    if (result != expectedResult) {
-        throw new Error(`${result} != ${expectedResult}`);
-    }
+    var z = {dog: 2};
+    expect(result).toBe(expectedResult);
+})
+it('should be that correct first name and las name comes from fullname', () => {
+    var result = utils.setName({}, "Stewart Boyd");
+    expect(result).toEqual({firstName: "Stewart", lastName: "Boyd"});
+
+})
+
+it("async square a number", (done) => {
+    utils.asyncSquare(3, (result) => {
+        expect(result).toBe(9);
+        done();
+    })
+})
+
+it("async add two numbers", (done) => {
+    utils.asyncAdd(3, 7, (result) => {
+        expect(result).toBe(10);
+        done();
+    })
 })
